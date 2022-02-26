fc608550675e71046063ba5f00c12715a095a4b9
Van Lam <levanlam3447@gmail.com>
2022-02-26 11:05:41 +0700

    Create NFT page

---
+++ src/screens/NFT/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+
+function Table({ columns, data }) {
+
+    // Render the UI for your table
+    return (
+        <>
+            NFT
+        </>
+    )
+}
+
+export default Table;
