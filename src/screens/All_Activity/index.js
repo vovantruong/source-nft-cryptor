e0275d350a6539944904a429d5726a60b3288baa
hieu294 <tranthehieu12>
2022-03-04 13:00:35 +0700

    update

--- src/screens/All_Activity/index.js
+++ src/screens/All_Activity/index.js
@@ -3,6 +3,7 @@ import styles from "./AllActivity.module.sass";
 import cn from "classnames";
 import Filters from "../Activity/Filters";
 import TableActivity from "./TableAcivity/TableActivity";
+import Icon from "../../components/Icon";
 
 
 const filters = [
@@ -18,10 +19,25 @@ const filters = [
 
 const AllActivity = () => {
   const [selectedFilters, setSelectedFilters] = useState([]);
+  const [visible, setVisible] = useState();
 
   return (
     <div className={cn("section", styles.body)}>
       <div className={cn("container", styles.container)}>
+        <div className={styles.top}>
+          <h1 className={cn("h2", styles.title)}>All Activity</h1>
+          <button
+            className={cn(
+              "button-circle-stroke button-small tablet-show",
+              styles.toggle,
+              { [styles.active]: visible }
+            )}
+            onClick={() => setVisible(!visible)}
+          >
+            <Icon name="filter" size="24" />
+            <Icon name="close" size="14" />
+          </button>
+        </div>
         <div className={styles.all__activity}>
           <Filters
             className={cn(styles.filters)}
@@ -30,7 +46,7 @@ const AllActivity = () => {
             setSelectedFilters={setSelectedFilters}
             close={true}
           />
-          <TableActivity className={styles.table}/>
+          <TableActivity className={styles.table} />
         </div>
       </div>
     </div>
