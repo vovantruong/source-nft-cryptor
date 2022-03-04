a0967ac67fac8bdc6ca41141c60a4811b5546592
hieu294 <tranthehieu12>
2022-03-04 17:04:41 +0700

    Creat page SellNFT

---
+++ src/screens/SellNFT/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import cn from "classnames";
+import styles from "./SellNFT.module.sass";
+
+const SellNFT = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SellNFT;
