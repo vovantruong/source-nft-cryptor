ee1a19e19cb0f7f7f8ce9fea50675f98c0968af9
hieu294 <tranthehieu12>
2022-03-04 11:21:06 +0700

    update file

--- src/screens/All_Activity/index.js
+++ src/screens/All_Activity/index.js
@@ -4,6 +4,7 @@ import cn from "classnames";
 import Filters from "../Activity/Filters";
 import Table from "../NFT/Table";
 import TableAllActivity from "./TableAllActivity/TableAllActivity";
+import Icon from "../../components/Icon";
 
 const filters = [
   "Sales",
@@ -18,21 +19,43 @@ const filters = [
 
 const AllActivity = () => {
   const [selectedFilters, setSelectedFilters] = useState([]);
+  const [visible, setVisible] = useState(0);
 
   return (
     <div className={cn("section", styles.body)}>
       <div className={cn("container", styles.container)}>
-      {/* <h1 className={cn("h2", styles.title)}>All Activity</h1> */}
-        <div className={styles.all__activity}>
-          <Filters
-            className={cn(styles.filters)}
-            filters={filters}
-            selectedFilters={selectedFilters}
-            setSelectedFilters={setSelectedFilters}
-            close={true}
-          />
-          <Table className={styles.table}/>
-        </div>
+      <div className={styles.all_activity}>
+            <h1 className={cn("h2", styles.title)}>Activity</h1>
+            {/* <button
+              className={cn(
+                "button-stroke button-small mobile-hide",
+                styles.button
+              )}
+            >
+              Mark all as read
+            </button> */}
+            <button
+              className={cn(
+                "button-circle-stroke button-small tablet-show",
+                styles.toggle,
+                { [styles.active]: visible }
+              )}
+              onClick={() => setVisible(!visible)}
+            >
+              <Icon name="filter" size="24" />
+              <Icon name="close" size="14" />
+            </button>
+          </div>
+          <div className={styles.row}>
+            <Filters
+              className={cn(styles.filters, { [styles.active]: visible })}
+              filters={filters}
+              selectedFilters={selectedFilters}
+              setSelectedFilters={setSelectedFilters}
+              close={true}
+            />
+            <Table />
+          </div>
       </div>
     </div>
   );
