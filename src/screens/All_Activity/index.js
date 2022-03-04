94cf8738252d4f0bc87b8a9bbadaf51278d83e2d
hieu294 <tranthehieu12>
2022-03-04 08:59:00 +0700

    upfile

--- src/screens/All_Activity/index.js
+++ src/screens/All_Activity/index.js
@@ -5,44 +5,35 @@ import Filters from "../Activity/Filters";
 import Table from "../NFT/Table";
 import TableAllActivity from "./TableAllActivity/TableAllActivity";
 
-// const filters = [
-//   "Sales",
-//   "Listings",
-//   "Bids",
-//   "Burns",
-//   "Followings",
-//   "Likes",
-//   "Purchase",
-//   "Transfers",
-// ];
-
-// const AllActivity = () => {
-//   // const [selectedFilters, setSelectedFilters] = useState([]);
-
-//   return (
-//     <div className={cn("section", styles.body)}>
-//       <div className={cn("container", styles.container)}>
-//       <h1 className={cn("h2", styles.title)}>All Activity</h1>
-//         <div className={styles.all__activity}>
-//           <Filters
-//             className={cn(styles.filters)}
-//             filters={filters}
-//             selectedFilters={selectedFilters}
-//             setSelectedFilters={setSelectedFilters}
-//             close={true}
-//           />
-//           <Table className={styles.table}/>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+const filters = [
+  "Sales",
+  "Listings",
+  "Bids",
+  "Burns",
+  "Followings",
+  "Likes",
+  "Purchase",
+  "Transfers",
+];
 
 const AllActivity = () => {
+  const [selectedFilters, setSelectedFilters] = useState([]);
 
   return (
     <div className={cn("section", styles.body)}>
-     
+      <div className={cn("container", styles.container)}>
+      {/* <h1 className={cn("h2", styles.title)}>All Activity</h1> */}
+        <div className={styles.all__activity}>
+          <Filters
+            className={cn(styles.filters)}
+            filters={filters}
+            selectedFilters={selectedFilters}
+            setSelectedFilters={setSelectedFilters}
+            close={true}
+          />
+          <Table className={styles.table}/>
+        </div>
+      </div>
     </div>
   );
 };
